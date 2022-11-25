393ceaee710889ebf91f260a8aa0e77c325cfd52
John Doe <shepherd.munemo@outlook.com>
2022-11-25 11:01:57 +0200

    Add project files.

---
+++ IMS.UseCases/ViewInventoriesByName.cs
@@ -0,0 +1,12 @@
+﻿using IMS.CoreBusiness;
+
+namespace IMS.UseCases
+{
+    public class ViewInventoriesByName
+    {
+        public async Task<IEnumerable<Inventory>> ExecuteAsync(string name)
+        {
+
+        }
+    }
+}
